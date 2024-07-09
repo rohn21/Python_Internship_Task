7de8e59906b761c42394456e39290368c69a1b95
Rohan_21 <rohn2100@gmail.com>
2024-07-09 14:51:07 +0530

    Creating a Python web server

--- webapp_task/webapp/urls.py
+++ webapp_task/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
+from webapp import views
 app_name = 'webapp'
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='webapp/index.html'), name='index'),
+    # path('',TemplateView.as_view(template_name='webapp/index.html'), name='index'),
+    path('', views.index, name='index'),
 ]
